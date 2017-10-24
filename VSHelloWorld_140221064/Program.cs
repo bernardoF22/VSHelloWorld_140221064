ae7d132e4dfd99fa60feba780f45aff68ab1b0dc
Bernardo F <bern_freitas@hotmail.com>
2017-10-24 22:42:49 +0100

    mais um comentário

--- VSHelloWorld_140221064/Program.cs
+++ VSHelloWorld_140221064/Program.cs
@@ -15,7 +15,7 @@ namespace VSHelloWorld_140221064
         public static void Main(string[] args)
         {
             //comentário de teste
-            //outro comentário
+            //novo comentário
             BuildWebHost(args).Run();
         }
 
